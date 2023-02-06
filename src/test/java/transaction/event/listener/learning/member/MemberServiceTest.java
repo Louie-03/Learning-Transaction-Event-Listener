11dd7484dba71ee119148c1573e19d24d743f9f6
Louie-03 <dhdustnr0134@naver.com>
2023-02-06 21:23:04 +0900

    test : 예외가 발생하지 않는 경우 정상적으로 @EventListener가 동작하는 것을 검증하는 테스트 구현

---
+++ src/test/java/transaction/event/listener/learning/member/MemberServiceTest.java
@@ -0,0 +1,29 @@
+package transaction.event.listener.learning.member;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @MockBean
+    private MemberEventListener memberEventListener;
+
+    @Test
+    void 만약_예외가_발생하지_않는다면_printMemberLog_메서드가_호출된다() {
+        memberService.join("memberA", false);
+
+        verify(memberEventListener).printMemberLog(any());
+    }
+
+}
