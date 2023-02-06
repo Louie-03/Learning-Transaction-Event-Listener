04aa5cb8e1638a118b2747339ab7a7750b3e6243
Louie-03 <dhdustnr0134@naver.com>
2023-02-06 21:31:09 +0900

    test : 회원 가입 실패 시 @EventListener 동작 테스트 구현
    예외가 발생하기 전에 이벤트를 발행했기 때문에 @EventListener가 동작한다.

--- src/test/java/transaction/event/listener/learning/member/MemberServiceTest.java
+++ src/test/java/transaction/event/listener/learning/member/MemberServiceTest.java
@@ -1,6 +1,8 @@
 package transaction.event.listener.learning.member;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.Test;
@@ -26,4 +28,12 @@ class MemberServiceTest {
         verify(memberEventListener).printMemberLog(any());
     }
 
+    @Test
+    void 회원가입에_실패해도_printMemberLog_메서드가_호출된다() {
+        assertThatThrownBy(() -> memberService.join("memberA", true))
+            .isInstanceOf(RuntimeException.class)
+            .hasMessage("예외가 발생했습니다.");
+
+        verify(memberEventListener, never()).printMemberLog(any());
+    }
 }
