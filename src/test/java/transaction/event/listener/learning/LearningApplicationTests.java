056a777b495177cfa405e7c22e9e98983850ca3f
Louie-03 <dhdustnr0134@naver.com>
2023-02-06 15:55:45 +0900

    Initial project

---
+++ src/test/java/transaction/event/listener/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package transaction.event.listener.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
